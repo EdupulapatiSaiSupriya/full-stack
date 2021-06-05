b59724a409cc6ab6dd1ea635530b65986246e770
EdupulapatiSaiSupriya <74001528+EdupulapatiSaiSupriya@users.noreply.github.com>
2021-06-05 23:16:17 +0530

    Add files via upload

---
+++ part 2/exercise 2.19/src/Components/Filters.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Filters = ({ onChange }) => {
+    return (
+        <div>
+            filter shown with <input onChange={onChange} />
+        </div>
+    )
+}
+
+export default Filters
